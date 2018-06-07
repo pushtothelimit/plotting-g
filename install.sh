52758b02516c6bd996b0afae21767793cf789606
Federico <federicobassi97@libero.it>
2018-06-07 21:51:44 +0200

    added install

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+
+QWT_VERSION=6.1.3
+QWT_SERVER=https://sourceforge.net/projects/qwt/files/qwt/
+QWT_DL_URL=$QWT_SERVER$QWT_VERSION"/qwt-"$QWT_VERSION".tar.bz2"
+QWT_PATH="/usr/local/qwt-"$QWT_VERSION
+
+#echo $QWT_DL_URL
+
+# Check if wget exist or not
+if ! [ -x "$(command -v wget)" ]; then
+	echo "Error, wget is not installed." >&2
+	exit 1
+fi
+
+echo "Downloading qwt-"$QWT_VERSION"..."
+mkdir /tmp/qwt/
+wget -P /tmp/qwt/ $QWT_DL_URL &>/dev/null
+
+echo "Compiling and installing qwt-"$QWT_VERSION"..."
+tar -xvjf qwt-$QWT_VERSION .tar.bz2
+cp /tmp/qwt/qwt-$QWT_VERSION $QWT_PATH
+rmdir /tmp/qwt/
+
+# now follow the qwt installation procedure
