0d7b557544679d4882f8c5b71d9ddf05e00bc0c5
Federico <federicobassi97@libero.it>
2018-06-10 09:20:09 +0200

    Finished design, changed slot names

--- include/MyWid.h
+++ include/MyWid.h
@@ -2,6 +2,12 @@
 #define MYWID_H
 
 #include <QWidget>
+#include <QApplication>>
+#include <QLineEdit>
+#include <QString>
+#include <QComboBox>
+
+#include <MyPlot.h>
 
 namespace Ui {
 class MyWid;
@@ -14,6 +20,22 @@ class MyWid : public QWidget
 public:
     explicit MyWid(QWidget *parent = 0);
     ~MyWid();
+
+   // MyPlot * plot1 ;
+//    QLineEdit * txte01;
+//    QLineEdit * txte02;
+//    QLineEdit * txte1;
+//    QLineEdit * txte2;
+//    QComboBox *combo2;
+//    QComboBox *combo01;
+    int gotnum;
+    int x,y,w,h;
+
+public slots:
+
+    void on_densityBtn_pressed();
+    int on_readFileBtn_pressed();
+    void on_exportBtn_pressed();
 
 private:
     Ui::MyWid *ui;
--- src/MyWid.cpp
+++ src/MyWid.cpp
@@ -1,6 +1,20 @@
 #include "MyWid.h"
 #include "ui_MyWid.h"
 
+#include <qwidget.h>
+#include <QStyle>
+#include <QBoxLayout>
+#include <qpushbutton.h>
+#include <QLabel>
+#include <QGroupBox>
+#include <QVector>
+#include <QTextStream>
+#include <QFile>
+
+#include <qwt_plot.h>
+#include <qwt_plot_curve.h>
+#include <qwt_series_data.h>
+
 MyWid::MyWid(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::MyWid)
@@ -12,3 +26,55 @@ MyWid::~MyWid()
 {
     delete ui;
 }
+
+
+void MyWid::on_densityBtn_pressed(){
+
+    /*
+            //*str=txte1->toPlainText();
+            bool ok;
+            gotnum=(txte1->displayText()).toInt(&ok,10);
+            plot1->listcrea(gotnum);
+            //std::cout<<gotnum<<'\n';
+    */
+        }
+
+int MyWid::on_readFileBtn_pressed(){
+    /*
+    int outVal = 0;
+    QString fileName;
+    fileName=txte02->displayText();
+    QTextStream out(stdout);
+    QFile file(fileName);
+    QTextStream in(&file);
+    int chk=file.open(QIODevice::ReadOnly);
+
+    if (!chk) {
+
+        out<<"File does not exist";
+        outVal = 1;
+    }
+    else {
+        out<<"ho aperto" << endl;
+        file.close();
+        plot1->listcreafile(fileName);
+    }
+    return outVal;
+    */
+}
+
+void MyWid::on_exportBtn_pressed(){
+    /*
+    QString * strName =new QString;
+    QString *strExt = new QString;
+    *strExt=combo2->currentText();
+    *strName=txte2->displayText();
+    strName->append('.');
+    strName->append(strExt);
+    QwtPlotRenderer *printer=new QwtPlotRenderer;
+    printer->renderDocument(plot1,*strName,*strExt,QSizeF( 300, 200 ), 85);
+    delete printer;
+    delete strName;
+    delete strExt;
+    */
+    }
